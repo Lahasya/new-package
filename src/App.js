5fbc321b302bd79f4b95ec38c84d7df8991b5d12
Lahasya Kr <lkr@global.sial.com>
2020-01-13 20:42:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './components/Card'
+function App() {
+  return (
+    <div className="App">
+     <Card>React</Card>
+    </div>
+  );
+}
+
+export default App;
